b9a99549576a3860a0ca89382295395b58427e36
beebelucas <beebelucas@gmail.com>
2021-11-17 18:08:25 -0600

    fixed again

--- sorting.h
+++ sorting.h
@@ -1,21 +1,11 @@
 #include<iostream>
-<<<<<<< HEAD
+
 using namespace std;
 
-#ifndef SORTING_H_
-#define SORTING_H_
-=======
 
-
->>>>>>> 05b57228db70930a9395d7da4a8400399490d330
 class Sorting{
     public:
     void selectionSort();
     void insertionSort();
-<<<<<<< HEAD
 };
 
-#endif
-=======
-};
->>>>>>> 05b57228db70930a9395d7da4a8400399490d330
