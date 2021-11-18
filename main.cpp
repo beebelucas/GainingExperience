ed8c216667b39e1ed89ce41ee0ff88b54473523c
beebelucas <beebelucas@gmail.com>
2021-11-17 18:01:29 -0600

    second commit

--- main.cpp
+++ main.cpp
@@ -1,2 +1,5 @@
-#include<iostream>
+#include"sorting.h"
 
+int main(){
+    
+}
---
+++ sorting.h
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+#ifndef SORTING_H_
+#define SORTING_H_
+class Sorting{
+    public:
+    void selectionSort();
+    void insertionSort();
+};
+
+#endif
